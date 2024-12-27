e17f9560f0e15bb58c49b6d7a3d531ceaf5435a4
tubakurouIT <tubakurou4maruti@yahoo.co.jp>
2024-12-27 08:46:27 +0000

    [Finish]応用7b

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,13 @@ class Book < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :week_favorites, -> { where(created_at: 1.week.ago.beginning_of_day..Time.current.end_of_day)}
 
+  
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
+  scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } #今週
+  scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } # 先週
+
+
 
 
   def self.serch_for(content, method)
